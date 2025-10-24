93fc88939cb99f94428ce7e8636a563b7a438693
huang710910-ops <huang710910@gmail.com>
2025-10-24 14:46:07 +0800

    push

---
+++ 11.py
@@ -0,0 +1,40 @@
+#練習1-1
+# def add_two_numbers(a,b):
+#     return a + b
+
+#練習1-2
+# def garden_area(radius):
+#     area = 3.14 * radius * radius
+#     return area
+# print(garden_area(5))
+
+#練習1-3
+# def add_two_numbers(a,b):
+#     if isinstance(a, int) and isinstance(b, int):
+#         return a + b
+#     else:
+#         return "請輸入整數"
+# print(add_two_numbers(3,5))
+
+#1-4
+# def convert_celsius_to_fahrenheit(celsius):
+#     fahrenheit = (celsius * 9/5) + 32
+#     return fahrenheit
+# print(convert_celsius_to_fahrenheit(25))
+
+# #1-5
+# def check_season(month):
+#     if month in [3, 4, 5]:
+#         return "春天"
+#     elif month in [6, 7, 8]:
+#         return "夏天"
+#     elif month in [9, 10, 11]:
+#         return "秋天"
+#     elif month in [12, 1, 2]:
+#         return "冬天"
+#     else:
+#         return "月份輸入錯誤"
+# a=input("請輸入月份:")
+# a=int(a)
+# month = a
+# print(check_season(a))
