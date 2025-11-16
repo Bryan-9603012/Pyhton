c0f7b2ea0cda8b694e8d6e174614eff7c3463907
huang710910-ops <huang710910@gmail.com>
2025-11-16 08:55:56 +0800

    Day16

---
+++ 16.py
@@ -0,0 +1,23 @@
+#1-1
+# from datetime import datetime 
+# now = datetime.now()
+# print(now)
+# day=now.day
+# month=now.month
+# year=now.year
+# hour=now.hour
+# minute=now.minute
+# second=now.second
+# timestamp=now.timestamp()
+# print(day,month,year,hour,minute,second,"\n")
+# print(timestamp,"\n")
+# print(f"{year}-{month}-{day} {hour}:{minute}:{second}","\n")
+#1-2
+# from datetime import datetime 
+# now = datetime.now()
+# t = now.strftime("%Y-%m-%d %H:%M:%S")
+# print("time:",t,"\n")
+# time_one = now.strptime(t,"%Y-%m-%d %H:%M:%S")
+# print("time_one:",time_one,"\n")
+# time_two = now.strftime("%Y-%d-%m %H:%M:%S")
+# print("time_two:",time_two,"\n")
